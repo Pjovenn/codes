d30a67b71c594629e0f2f6bbae9c71c5b997ab62
Pjovenn <126535793+Pjovenn@users.noreply.github.com>
2023-03-01 16:34:23 +0800

    Update thirdmax2.cpp

--- thirdmax2.cpp
+++ thirdmax2.cpp
@@ -12,7 +12,22 @@ int arr[] = {1,2,3,4,5,6,7,8,9,10}; //initialize array
 int len = sizeof(arr)/sizeof(arr[0]); //determine the size of array sizeofarr(4*10) / sizeofarr of each element(4)
 
 sort(arr, arr+len, greater<int>()); //sorting algorithm that sorts elements in a descending order
+    
 
+    /* //bubble sort algorithm
+for(int i=0; i<n-1; i++) //loops 10 times from 0 to 9
+{
+    for(int j=0;j<n-i-1; j++) // loop count is decreased as i increases
+    {
+        if(arr[j]<arr[j+1]) // implements only when previous element is less than element, swapping as index(j) increases
+        {
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+    }
+}
+    */
 //optional
 for(int i=0; i<len; i++) // prints out all the elements in descending order
 {
