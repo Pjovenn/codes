027941ced557d05cfd4c517aee930c104463eea8
Pjovenn <126535793+Pjovenn@users.noreply.github.com>
2023-03-04 13:00:22 +0800

    Update Tmax.cpp

--- Tmax.cpp
+++ Tmax.cpp
@@ -37,13 +37,38 @@ int CopyThird()
 
 int CompThird()
 {
-    int arr[] = {11,12,13,14,15,16,17,18,19,20}; //Initialized array
-    int len = sizeof(arr) / sizeof(arr[0]); //Number of elements in the array
+    int arr[] = {21,28,23,27,24,25,26,22,29}; //Initialized array
+    int len = sizeof(arr)/ sizeof(arr[0]); //Number of elements in the array
+    sort(arr, arr+len); //Sort elements in ascending order
+    int maxA = arr[0], maxB = arr[1], maxC = arr[2]; //Initialize maxA,maxB,maxC to first 3 elements
 
+    for(int i = 1; i<=(len/3); i++) //Loops from 1 to 3
+    { 
+        if((i*3)!= len) //avoids comparing elements to non-existing elements in the array
+        {
+            if(maxA < arr[(i*3)] && maxB < arr[(i*3)+1] && maxC < arr[(i*3)+2]) //compares maxA,maxB,maxC to the three next elements 
+            {
+                maxA = arr[(i*3)];  //Assign the values to the three maxes
+                maxB = arr[(i*3)+1];
+                maxC = arr[(i*3)+2];
+            }
+        }
+
+    }
 
     cout << "The third max value is: ";
-
-    return ;
+    if(maxA < maxB && maxA < maxC)  //Returns maxA as the lowest value of the three maxes
+    {
+        return maxA;
+    }
+    else if(maxB < maxA && maxB < maxC) //Returns maxB as the lowest value of the three maxes
+    {
+        return maxB;
+    }
+    else    //Returns maxC as the lowest value of the three maxes
+    {
+        return maxC;
+    }
 }
 
 
