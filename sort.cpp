49b152e56e84a148c4dff6c800c68b1d45e856d2
Pjovenn <126535793+Pjovenn@users.noreply.github.com>
2023-03-26 21:28:47 +0800

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -45,7 +45,20 @@ void insort()
             arr[pos] = curr;
         }
     }
-
+    //shell
+void shellSort(int arr[], int n) {
+    for (int gap = n/2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int j = i;
+            int temp = arr[i];
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+}
 cout << endl;
     for(int i =0; i<n; i++)
     {
